10ecb9522976abf2e6f88ca9ec7a19d9e8bc0292
Daniel Lerch <daniel@lerchpanther.ch>
2019-07-11 17:07:03 +0200

    ot dot

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,51 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <main>
+    <section>
+      <svg class="dot" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 120" version="1.1">
+        <circle />
+      </svg>
+      
+      <h1>MEDIN</h1>
+      <h2>Biel / Bienne</h2>
+    </section>
+  </main>
 </template>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+html, body, main
+  width: 100%
+  height: 100%
+  margin: 0
+  padding: 0
+  font-family: 'Jost'
+main 
+  position: relative
+section
+  position: absolute
+  top: 50%
+  left: 50%
+  transform: translate(-50%, -50%)
+  // background-color pink
+  // background-clip: text
+  // color: transparent
+  h1
+    color: black
+    font-size: 6rem
+    margin: 0
+    line-height: 1
+  .dot 
+    position: absolute
+    left: 13.55rem
+    top: -2rem
+    height: 1.5rem
+    circle 
+      fill: red
+      cx: 60
+      cy: 60
+      r: 50
+  h2 
+    margin: 0
+    font-size: 2.5rem
+    font-weight: 400
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+
 </style>
